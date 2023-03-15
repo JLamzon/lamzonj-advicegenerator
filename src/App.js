6493b878f20fb6241bb750161ccb183e36776b92
JLamzon <jlamzon@yahoo.com>
2023-03-14 22:08:27 -0700

    Added peer review

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+//Jessie Lamzon
+//3/14/23
+//Project: Advice Generator
+// Peer Review by: Pedro Castaneda - Excellent job on making it look exactly like the design from frontendmentor.io! Also great job on using letter-spacing on the advice slip ID text. I'm still trying to understand how you switch between dividers when on mobile or desktop. I struggled with that one. The only thing I would suggest is going back and see if you can get the correct quotes to appear and also changing the favicon from the react icon to the frontendmentor icon provided in the images folder. Other than that, great great work!
+
+
 import './App.css';
 import AdviceApi from './components/AdviceApi';
 

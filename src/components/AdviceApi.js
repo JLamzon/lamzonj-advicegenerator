fd1093b827321ec63dfd8a935668e6ef2f00acf7
JLamzon <jlamzon@yahoo.com>
2023-03-14 20:15:04 -0700

    set divider to fixed position

--- src/components/AdviceApi.js
+++ src/components/AdviceApi.js
@@ -9,16 +9,18 @@ function AdviceApi() {
   const [generatedAdvice, setGeneratedAdvice] = useState("");
   const [generatedNumber, setGeneratedNumber] = useState();
 
+
   async function fetchAdvice() {
     const result = await fetch("https://api.adviceslip.com/advice");
     const data = await result.json();
+    
     const advice = data.slip.advice;
     const adviceNumber = data.slip.id;
+
 
     setGeneratedAdvice(advice);
     setGeneratedNumber(adviceNumber);
   }
-
 
 
   //loads the fetch when page opens.  great if yu have a website that fetches prompt from an api
@@ -43,8 +45,8 @@ function AdviceApi() {
         </Row>
         <Row className="divider">
           <Col>
-            <img className="dividerDesktop" src={dividerDesktop} alt="middle divider"></img>
-            <img className="dividerMobile" src={dividerMobile} alt="middle divider"></img>
+            <img className="dividerDesktop dividerPosition" src={dividerDesktop} alt="middle divider"></img>
+            <img className="dividerMobile dividerPosition" src={dividerMobile} alt="middle divider"></img>
           </Col>
         </Row>
         <Row>
